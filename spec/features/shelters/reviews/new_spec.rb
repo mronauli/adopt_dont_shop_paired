afaffa166aa2e1552f31c3df78e86103036a882a
Maria Ronauli <mronauli@gmail.com>
2020-02-04 16:54:35 -0700

    add tests for review creation

--- spec/features/shelters/reviews/new_spec.rb
+++ spec/features/shelters/reviews/new_spec.rb
@@ -1,27 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe 'As a visitor' do 
+RSpec.describe 'As a visitor' do
     describe "When I visit a shelter's show page" do
-        it "I see a like to 'Add Review' and it directs me to a new review form " do  
+        it "I see a like to 'Add Review' and it directs me to a new review form " do
             shelter_1 = Shelter.create!(name: "Dumb Friends League", address: "123 Sample St", city: "Denver", state: "CO", zip: 80220)
 
             visit shelter_path(shelter_1)
 
-            expect(page).to have_link('Add Review')
+            click_link('Add Review')
             expect(current_path).to eq("/shelters/#{shelter_1.id}/reviews/new")
 
             title = "Very bad!"
             rating = 1
             content = "Molly came back skinny"
 
-            fill_in 'review[title]', with: title 
-            fill_in 'review[rating]', with: rating 
-            fill_in 'review[content]', with: content 
-            
-            click_on "Submit Review"
-            expect(current_path).to eq(shelter_path(shelter_1))
+            fill_in 'title', with: title
+            fill_in 'rating', with: rating
+            fill_in 'content', with: content
 
-            within("#review-#{shelter_1.review.id}") do
+            click_on "Submit Review"
+
+            expect(current_path).to eq("/shelters/#{shelter_1.id}")
+
+            review_1 = shelter_1.reviews[0]
+
+            within("#review-#{review_1.id}") do
                 expect(page).to have_content(review_1.title)
                 expect(page).to have_content(review_1.rating)
                 expect(page).to have_content(review_1.content)
