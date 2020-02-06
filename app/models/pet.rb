a8e7d367a41ebd7fd4f96d4b082a4ed39fe9683e
Maria Ronauli <mronauli@gmail.com>
2020-02-06 16:48:01 -0700

    add has many relationship to pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,7 @@
 class Pet < ApplicationRecord
    validates_presence_of :name, :image, :approximate_age, :sex, :description
-   
+
    belongs_to :shelter
+   has_many :pet_applications
+   has_many :applications, through: :pet_applications
 end
