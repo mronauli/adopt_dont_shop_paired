a990b632d7b26416a60dab929dd1e6737d17c2ef
Maria Ronauli <mronauli@gmail.com>
2020-02-06 18:23:04 -0700

    add non-functioning destroy method

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,20 @@
 class FavoritesController < ApplicationController
+  def index
+    @pets = Pet.all
+  end
 
-    def index 
-        @pets = Pet.all 
-    end 
+  def update
+    pet = Pet.find(params[:id])
+    favorite.add_pet(pet.id)
+    session[:favorite] ||= favorite.contents
+    quantity = favorite.count_of(pet.id)
+    flash[:success] = "#{pet.name} has been added to your favorites"
+    redirect_to "/pets/#{pet.id}"
+  end
 
-    def update
-        pet = Pet.find(params[:id])
-        favorite.add_pet(pet.id)
-        session[:favorite] ||= favorite.contents
-        quantity = favorite.count_of(pet.id)
-        flash[:success] = "#{pet.name} has been added to your favorites"
-        redirect_to "/pets/#{pet.id}"
-    end
-end 
+  def destroy
+    pet = Pet.find(params[:id])
+    favorite.delete_pet(pet.id)
+    redirect_to "/pets"
+  end
+end
