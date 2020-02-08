7d11d6cf2fe06e749756f959b01d83cbf9c06a17
Maria Ronauli <mronauli@gmail.com>
2020-02-08 10:11:30 -0700

    add changes from master

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,21 @@
 class FavoritesController < ApplicationController
+  def index
+    @pets = Pet.all
+  end
 
-    def index 
-        @pets = Pet.all 
-    end 
+  def update
+    pet = Pet.find(params[:id])
+    favorite.add_pet(pet.id)
+    session[:favorite] ||= favorite.contents
+    quantity = favorite.count_of(pet.id)
+    flash[:success] = "#{pet.name} has been added to your favorites"
+    redirect_to "/pets/#{pet.id}"
+  end
 
-    def update
-        pet = Pet.find(params[:id])
-        favorite.add_pet(pet.id)
-        session[:favorite] ||= favorite.contents
-        quantity = favorite.count_of(pet.id)
-        flash[:success] = "#{pet.name} has been added to your favorites"
-        redirect_to "/pets/#{pet.id}"
-    end
-end 
+  def destroy
+    pet = Pet.find(params[:id])
+    favorite.delete_pet(pet.id)
+    flash[:success] = "#{pet.name} has been removed from your favorites."
+    redirect_back(fallback_location: "/pets/#{pet.id}")
+  end
+end
