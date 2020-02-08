9420e4b3ea63e60a2f65a91d1b359dca4ec28c62
Maria Ronauli <mronauli@gmail.com>
2020-02-08 16:21:03 -0700

    remove pet names helper

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,8 +2,4 @@ class Application < ApplicationRecord
   validates_presence_of :name, :address, :city, :state, :zip, :phone_number, :description
   has_many :pet_applications
   has_many :pets, through: :pet_applications
-
-  def pet_names
-    require "pry"; binding.pry
-  end
 end
