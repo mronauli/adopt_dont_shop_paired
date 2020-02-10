6aa3a9a6c3ee00f050614c6b6cf917b36c4fbcc2
Maria Ronauli <mronauli@gmail.com>
2020-02-09 18:42:29 -0700

    add routes for pet applications

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,8 @@ Rails.application.routes.draw do
   delete '/favorites/:id', to: 'favorites#destroy'
 
   get '/applications/new', to: 'applications#new'
+  get '/applications/:id', to: 'applications#show'
   post '/applications', to: 'applications#create'
+
+  get 'pet_applications/:id', to: 'pet_applications#index'
 end
