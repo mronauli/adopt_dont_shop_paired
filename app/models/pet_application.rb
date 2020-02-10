f4e1c209ddee5b6a0ad88f6ed08cc92b581f2c8d
Maria Ronauli <mronauli@gmail.com>
2020-02-09 18:46:26 -0700

    add helper method

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -1,4 +1,8 @@
 class PetApplication < ApplicationRecord
   belongs_to :pet
   belongs_to :application
+
+  def people_names
+    PetApplication.joins(:application).map(&:application).map(&:name).uniq
+  end
 end
