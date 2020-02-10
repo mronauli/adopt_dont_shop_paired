612fe64ccbd0d0890f2d2fbe392e90aae3d7f740
Maria Ronauli <mronauli@gmail.com>
2020-02-09 18:45:06 -0700

    edit create method

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,24 +1,29 @@
 class ApplicationsController < ApplicationController
+  def show
+    @application = Application.find(params[:id])
+  end
+
   def new
     favorited_pets = favorite.contents.keys
     @pets = Pet.find(favorited_pets)
   end
 
   def create
-    favorited_pets = favorite.keys
-    @pet_applications = Pet.find(favorited_pets)
-    @pet_applications.each do |pet| 
+    @applied_for = Pet.find(params[:pets])
+    @applied_for.each do |pet|
       pet.applications.new(application_params)
     end
-    case @pet_applications.all? { |pet_app| pet_app.save } 
+    case @applied_for.map do |pet_app|
+      pet_app.save
+      end
       when true
         session[:favorite].clear
-        flash[:success] = "Application has been submitted successfully!" 
+        flash[:success] = "Application has been submitted successfully!"
         redirect_to "/favorites"
-      else 
+      else
         flash[:alert] = "Please enter information for all fields."
         redirect_to "/applications/new"
-    end 
+    end
   end
 
   private
