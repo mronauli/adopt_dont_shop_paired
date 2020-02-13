655e53b02322f24c14f487019761a9fc767b71a2
Daniel <danmoran.pro@gmail.com>
2020-02-12 18:37:35 -0700

    Delete spec error fixec

--- spec/features/pets/delete_spec.rb
+++ spec/features/pets/delete_spec.rb
@@ -63,7 +63,6 @@ RSpec.describe 'As a visitor' do
           end
           visit "/pets/#{@pet_1.id}"
 
-
           click_link "Delete Pet"
 
           visit pets_path
@@ -72,7 +71,7 @@ RSpec.describe 'As a visitor' do
             expect(page).to have_content("Favorites: 1")
           end
 
-          within("#pet-#{@pet_1.id}") do
+          within("#pet-#{@pet_2.id}") do
             click_link "Delete Pet"
           end
 
